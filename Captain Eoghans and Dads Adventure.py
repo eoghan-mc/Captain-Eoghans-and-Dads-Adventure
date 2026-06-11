f37408bb1491c95784ff2342d893d90bc1e8b880
eoghan-mc <eoghan.macmahuna@gmail.com>
2026-06-11 10:00:51 +0100

    Update Captain Eoghans and Dads Adventure.py
    add some rooms and directions

--- Captain Eoghans and Dads Adventure.py
+++ Captain Eoghans and Dads Adventure.py
@@ -7,11 +7,18 @@ Adventure game
 https://github.com/ericclack/python-text-adventure/blob/main/adventure3c.py
 """
 
-from IPython import get_ipython
+import os
+
+# This clears the terminal screen (uses 'cls' for Windows and 'clear' for Mac/Linux)
+os.system('cls' if os.name == 'nt' else 'clear')
+
 import time
-get_ipython().magic('clear')
+
+
 
 def dots(number):
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     dotis = "..........."
     
@@ -36,9 +43,6 @@ spung = input("what is your name? \n")
 
 person =Adventurer(spung)
 
-from IPython import get_ipython
-get_ipython().magic('clear')
-
 dots(6)
            
 print("\n\nHello " + person.Name +".  You are a guest.  People enter this hotel but never leave!!!!\n")
@@ -48,13 +52,38 @@ print("\n\nHello " + person.Name +".  You are a guest.  People enter this hotel 
 def reception():
   print("\n")
   print("You are standing in the reception of a boutique hotel.  \
-There is no door and you do not know how you got into the reception.")
+There is no entrance door and you do not know how you got into the reception.")
   print("On the reception desk, there is a brass lamp with a green shade and \
 underneath stands a sign saying go WEST")
   print("To the North you can see a gloomy room with a yellowish light in one corner.  ")
   print("\nTo the South you can see a gloomy room with a orangeish light in one corner.\nTo the East is a wall covered with moss and snakes.  \nTo the West you can see a gloomy room with a orangeish light in one corner.  ")
 
+  direction = input("What way would you like to go?(N S E W) \n")
+  
+  if direction == "N":
+      marvellousroom()
+  elif direction == "E":
+      snakeroom()
+  elif direction == "S":
+      orange()
+  else :
+      print("Gaboom! You put in a wrong letter!  ")
+      reception()
       
+
+def marvellousroom():
+    dots(2)
+    print("\n")
+    print("You are standing in the most marvellous room.  You understand life the universe and everything. Your life is complete!  ")
+
+def orange():
+    print("There are 3 differnt doors 1 of the doors ")
+
+def snakeroom():
+    print("\n")
+    print("You are eaten by snakes.  you are dead!  ")
+    
+
 def player_action(directions )     :
     
     x=10
